41237154f3f0acc1976fc197172e7e856e02cc30
kassem <omarksm09@gmail.com>
2022-12-15 21:25:06 +0200

    resolve decompress errors

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ ENV DEBIAN_FRONTEND=noninteractive \
 RUN apt-get update -y && apt-get install  -y --fix-missing openssh-server wget python3.10 python3-pip && pip install redis
 RUN wget https://github.com/threefoldtech/zinit/releases/download/v0.2.10/zinit -O /sbin/zinit &&\
 	chmod +x /sbin/zinit
-
 ADD rootfs /
 ADD listener.py /
 CMD ["/sbin/zinit", "init", "--container"]
--- listener.py
+++ listener.py
@@ -1,19 +1,22 @@
 import os
 import redis
 import gzip
+import json
 
-
-redis_conn = redis.Redis(charset="utf-8", decode_responses=True)
+redis_conn = redis.Redis(charset="utf-8", decode_responses=False)
 from multiprocessing import Process
 
 def sub(name: str):
+
     pubsub = redis_conn.pubsub()
     pubsub.subscribe("zlog")
+    print("[+] Subscribed....")
     for message in pubsub.listen():
-        if message.get("type") == "message":
+         if message.get("type") == "message":
             data = message.get("data")
-            newdata = gzip.decompress(data).decode('utf-8')
-            f = open("output.txt", "a")
-            f.write(newdata)
+            decompressedData =gzip.decompress(data).decode('utf-8')
+            f = open("/output.txt", "a")
+            f.write(decompressedData)
             f.close()
-Process(target=sub, args=("reader1a",)).start()
+
+Process(target=sub, args=("reader",)).start()
