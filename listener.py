a2020320ef27fb93475b6c98b94a7dfe2076e546
kassem <omarksm09@gmail.com>
2022-12-21 14:36:36 +0200

    remove time logger

--- listener.py
+++ listener.py
@@ -1,8 +1,6 @@
 import os
 import redis
 import gzip
-import json
-from datetime import datetime
 redis_conn = redis.Redis(charset="utf-8", decode_responses=False)
 from multiprocessing import Process
 
@@ -16,8 +14,6 @@ def sub(name: str):
             data = message.get("data")
             decompressedData =gzip.decompress(data).decode('utf-8')
             f = open("/zlogOutput.txt", "a")
-            current_time=datetime.now()
-            f.write("\n ---------------------"+current_time.strftime("%d/%m/%Y %H:%M:%S")+"------------------------\n")
             f.write(decompressedData)
             f.close()
 
